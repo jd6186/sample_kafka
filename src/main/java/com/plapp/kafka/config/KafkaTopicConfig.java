f00e98ffaba23666d88506bd0fea605dd74369e5
jeongdong-ug <wjdehd7991@gmail.com>
2024-08-05 14:41:06 +0900

    feat: topic이 없을 경우 topic을 생성하게 만들도록 Topic bean 생성

--- src/main/java/com/plapp/kafka/config/KafkaTopicConfig.java
+++ src/main/java/com/plapp/kafka/config/KafkaTopicConfig.java
@@ -1,23 +1,36 @@
 package com.plapp.kafka.config;
 
 import com.plapp.kafka.code.ConsumerGroupTypeCode;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+@EnableKafka
 @Configuration
 public class KafkaTopicConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
     @Value("${spring.kafka.consumer.auto-offset-reset}")
     private String autoOffsetReset;
+
+    @Bean
+    public NewTopic AdManager() {
+        return new NewTopic("ad-manager", 10, (short) 3);
+    }
+
+    @Bean
+    public NewTopic AdScheduledDataTransfer() {
+        return new NewTopic("ad-scheduled-data-transfer", 10, (short) 3);
+    }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactoryA() {
